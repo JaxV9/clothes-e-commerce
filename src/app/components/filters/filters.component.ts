aa3e976818cfc76a3618799538a06c40ae717dab
JaxV9 <layanj.pro@gmail.com>
2025-10-19 12:27:19 +0200

    feat: filter and back-button

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  inject,
+  signal,
+} from '@angular/core';
+import { BackComponent } from '../ui/buttons/back/back.component';
 
 @Component({
   selector: 'app-filters',
-  imports: [],
+  imports: [CommonModule, BackComponent],
   templateUrl: './filters.component.html',
-  styleUrl: './filters.component.css'
+  styleUrl: './filters.component.css',
 })
 export class FiltersComponent {
+  elementRef = inject(ElementRef);
+  filterIsDisplay = signal<boolean>(false);
 
+  toggleFilter(): void {
+    this.filterIsDisplay.update((prev) => !prev);
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside && this.filterIsDisplay()) {
+      this.toggleFilter();
+    }
+  }
 }
--- src/app/pages/clothes/clothes.component.ts
+++ src/app/pages/clothes/clothes.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { ClothesCardListComponent } from '../../components/clothes-card-list/clothes-card-list.component';
 import { FiltersComponent } from '../../components/filters/filters.component';
+import { BackComponent } from '../../components/ui/buttons/back/back.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-clothes',
-  imports: [ClothesCardListComponent, FiltersComponent],
+  imports: [
+    ClothesCardListComponent,
+    FiltersComponent,
+    BackComponent,
+    RouterModule,
+  ],
   templateUrl: './clothes.component.html',
   styleUrl: './clothes.component.css',
 })
