f700161fe69f6c366cc308b607af43a1e0287a19
JaxV9 <layanj.pro@gmail.com>
2025-11-03 14:42:17 +0100

    style: back button

--- src/app/components/ui/buttons/back/back.component.ts
+++ src/app/components/ui/buttons/back/back.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-back',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './back.component.html',
-  styleUrl: './back.component.css'
+  styleUrl: './back.component.css',
 })
 export class BackComponent {
-
+  position = input<'left' | 'right'>('left');
 }
