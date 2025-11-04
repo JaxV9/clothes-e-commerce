591daa92657b17ed5810f70a28a501b2c4acf98b
JaxV9 <layanj.pro@gmail.com>
2025-11-04 11:14:39 +0100

    feat: get tags from clothes

--- src/app/services/clothes-service/clothes.service.ts
+++ src/app/services/clothes-service/clothes.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { QuickHttp, ResAction } from '@jaslay/http';
-import { Clothe } from '../../../models/models';
+import { Clothe, Tag } from '../../../models/models';
 import { StoreService } from '../store-service/store.service';
 import { environment } from '../../../environment';
-import { fakeClothes } from './fakeData';
 
 @Injectable()
 export class ClothesService {
@@ -16,16 +15,31 @@ export class ClothesService {
   async loadClothes() {
     const response: ResAction = await this.quickHttp.get('clothes/');
     const payload = response.payload as Clothe[];
-    const result = Array.from({ length: 12 }, (_, index) => ({
+    const clothes = Array.from({ length: 12 }, (_, index) => ({
       ...payload[index % 2 === 0 ? 1 : 0],
       id: index,
     }));
-    this.storeService.addItem('clothes', result);
+    const tags = this.getTagsFromClothes(clothes);
+    this.storeService.addItem('clothes', clothes);
+    this.storeService.addItem('tags', tags);
+    console.log(this.storeService.store());
   }
 
   getClothes(): Clothe[] {
     const clothes = this.storeService.store()['clothes'] as Clothe[];
     return clothes;
+  }
+
+  getTagsFromClothes(clothes: Clothe[]): Tag[] {
+    const tags: Tag[] = [];
+    clothes.map((clothe) => {
+      clothe.tags.map((tag) => {
+        if (!tags.includes(tag.name)) {
+          tags.push(tag.name);
+        }
+      });
+    });
+    return tags;
   }
 
   constructor(private readonly storeService: StoreService) {}
